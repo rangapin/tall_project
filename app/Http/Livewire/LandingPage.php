90784ad8f4a04cdb27cc695170550aa5169b4124
rangapin <rangapin@gmx.com>
2022-12-14 00:00:47 -0600

    git init

---
+++ app/Http/Livewire/LandingPage.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Subscriber;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Livewire\Component;
+
+class LandingPage extends Component
+{
+    public $email;
+    public $showSubscribe = false;
+    public $showSuccess = false;
+
+    protected $rules = [
+        'email' => 'required|email:filter|unique:subscribers,email',
+    ];
+
+    public function mount(Request $request)
+    {
+        if ($request->has('verified') && $request->verified == 1) {
+            $this->showSuccess = true;
+        }
+    }
+
+    public function subscribe()
+    {
+        $this->validate();
+
+        DB::transaction(function() {
+            $subscriber = Subscriber::create([
+                'email' => $this->email,
+            ]);
+
+            $notification = new VerifyEmail;
+            $notification->createUrlUsing(function($notifiable) {
+                return URL::temporarySignedRoute(
+                    'subscribers.verify',
+                    now()->addMinutes(30),
+                    [
+                        'subscriber' => $notifiable->getKey(),
+                    ],
+                );
+            });
+
+            $subscriber->notify($notification);
+        }, $deadlockRestries = 5);
+
+
+        $this->reset('email');
+        $this->showSubscribe = false;
+        $this->showSuccess = true;
+    }
+
+    public function render()
+    {
+        return view('livewire.landing-page');
+    }
+}
